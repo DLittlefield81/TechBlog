58618b414b003effc63cc92af3ca444918d8b467
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-06-28 21:41:29 -0400

    updated controllers and views

---
+++ public/js/dashboard.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#blog-name').value.trim();
+  const needed_funding = document.querySelector('#blog-funding').value.trim();
+  const description = document.querySelector('#blog-desc').value.trim();
+
+  if (name && needed_funding && description) {
+    const response = await fetch(`/api/blogs`, {
+      method: 'POST',
+      body: JSON.stringify({ name, needed_funding, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-blog-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.blog-list')
+  .addEventListener('click', delButtonHandler);
